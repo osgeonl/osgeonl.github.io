d2f0ebc9313e9aa380fad507d6df513fb9ec46fd
Just van den Broecke <just@justobjects.nl>
2014-08-22 18:50:34 +0200

    sitecontent, OSGeo.nl Dag verslagen, presentaties etc toevoegen

---
+++ sitecontent/osgeonl_dag/media/sprekers-osgeodag-2012/convert.sh
@@ -0,0 +1,22 @@
+height=76
+convert -resize x$height paul-van-genugten.jpg paul-van-genugten-s.jpg
+convert -resize x$height edward-mac-gillavry.jpg edward-mac-gillavry-s.jpg
+convert -resize x$height pieter-meijer.jpg pieter-meijer-s.jpg
+convert -resize x$height just-van-den-broecke.jpg just-van-den-broecke-s.jpg
+convert -resize x$height steven-ottens.jpg steven-ottens-s.jpg
+convert -resize x$height jwvanaalst.png jwvanaalst-s.jpg
+convert -resize x$height thijs-van-menen.jpg thijs-van-menen-s.jpg
+convert -resize x$height milo-nieuw.jpg milo-van-der-linden-s.jpg
+convert -resize x$height map-jw.jpg map-jw-s.jpg
+convert -resize x$height fossgis-de-2012.jpg fossgis-de-2012-s.jpg
+convert -resize x$height willy-bakker.jpg willy-bakker-s.jpg
+convert -resize x$height just-gwf-2012.jpg just-gwf-2012-s.jpg
+convert -resize x$height bof-gin-congres-2011.jpg bof-gin-congres-2011-s.jpg
+convert -resize x$height herman-assink.jpg herman-assink-s.jpg
+convert -resize x$height henk-hoff.jpg henk-hoff-s.jpg
+convert -resize x$height roy-braam.jpg roy-braam-s.jpg
+convert -resize x$height erik-romijn.jpg erik-romijn-s.jpg
+convert -resize x$height marc-vloemans.jpg marc-vloemans-s.jpg
+convert -resize x$height gert-jan.jpg gert-jan-s.jpg
+convert -resize x$height robert-lievers.jpg robert-lievers-s.jpg
+
